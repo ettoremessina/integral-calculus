cdd1daac688236dc03a78af325b82e2af0995e18
Ettore Messina <et.messina@gmail.com>
2021-09-03 23:05:31 +0200

    minor fix

--- demo/python/scipy/scipy-integr1-04-quad-arclength.py
+++ demo/python/scipy/scipy-integr1-04-quad-arclength.py
@@ -4,7 +4,7 @@ import autograd as ag
 
 print('Compute length of an arc of planar parametric curve by SciPy quad')
 print('Example 1-04 quad arclength')
-print('Length of arc of parametric curve x(t)=cos^3(t) and y(t)=sin^3(t) from x=0 to x=2pi')
+print('Length of arc of parametric curve x(t)=cos^3(t) and y(t)=sin^3(t) from t=0 to t=2pi')
 
 x = lambda t : anp.cos(t) ** 3
 y = lambda t : anp.sin(t) ** 3
